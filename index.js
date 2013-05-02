6417cdcd90ac712f8a5f3f849f93518cc59cac52
James Halliday <mail@substack.net>
2013-05-01 22:17:20 -0700

    use a createStream() function instead of .stream

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ var nextTick = typeof setImmediate !== 'undefined'
 
 exports = module.exports = (function () {
     var harness = createHarness();
-    harness.stream.pipe(createDefaultStream());
+    harness.createStream().pipe(createDefaultStream());
     return harness;
 })();
+
 exports.createHarness = createHarness;
 exports.Test = Test;
 exports.test = exports; // tap compat
@@ -27,9 +28,14 @@ var exitInterval;
 
 function createHarness (conf_) {
     var exitCode = 0;
-    var results = createResultStream();
+    var results;
     
     var test = function (name, conf, cb) {
+        if (!results) {
+            results = createResultStream();
+            results.pause();
+        }
+        
         var t = new Test(name, conf, cb);
         (function inspectCode (st) {
             st.on('test', function sub (st_) {
@@ -43,6 +49,12 @@ function createHarness (conf_) {
         results.push(t);
         return t;
     };
-    test.stream = results;
+    
+    test.createStream = function () {
+        if (!results) results = createResultStream();
+        nextTick(function () { results.resume() });
+        return results;
+    };
+    
     return test;
 }
