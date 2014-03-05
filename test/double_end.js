45add173cf5ed739fae31a90c64eb3d8b308260e
James Halliday <mail@substack.net>
2014-03-04 21:03:20 -0800

    complete double end test

--- test/double_end.js
+++ test/double_end.js
@@ -1,34 +1,27 @@
-var tape = require('../');
-var tap = require('tap');
+var test = require('tap').test;
 var concat = require('concat-stream');
+var spawn = require('child_process').spawn;
 
-tap.test(function (t) {
-    t.plan(1);
-    
-    var test = tape.createHarness();
-    var tc = tap.createConsumer();
-    
-    var rows = [];
-    tc.on('data', function (row) { rows.push(row) });
-    tc.on('end', function () {
-        t.deepEqual(rows, [
+test(function (t) {
+    t.plan(2);
+    var ps = spawn(process.execPath, [ __dirname + '/double_end/double.js' ]);
+    ps.on('exit', function (code) {
+        t.equal(code, 1);
+    });
+    ps.stdout.pipe(concat(function (body) {
+        t.equal(body.toString('utf8'), [
             'TAP version 13',
-            'double end',
-            { id: 1, ok: true, name: 'should be equivalent' },
-            { id: 2, ok: false, name: '.end() called twice' },
-            'tests 2',
-            'pass  1',
-            'fail  1',
-        ]);
-    });
-    
-    test.createStream().pipe(tc);
-    
-    test('double end', function (tt) {
-        tt.equal(1 + 1, 2);
-        tt.end();
-        setTimeout(function () {
-            tt.end();
-        }, 5);
-    });
+            '# double end',
+            'ok 1 should be equal',
+            'not ok 2 .end() called twice',
+            '  ---',
+            '    operator: fail',
+            '  ...',
+            '',
+            '1..2',
+            '# tests 2',
+            '# pass  1',
+            '# fail  1',
+        ].join('\n') + '\n\n');
+    }));
 });
