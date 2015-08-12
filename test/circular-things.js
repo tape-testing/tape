b73d2bfb69364f5332f5958bca2c69099aedc290
Michael Hayes <mhayes@newrelic.com>
2015-08-12 12:13:46 -0700

    improve yaml formatting of diagnostic information

--- test/circular-things.js
+++ test/circular-things.js
@@ -1,43 +1,36 @@
 var tape = require('../');
 var tap = require('tap');
+var concat = require('concat-stream');
 
 tap.test('circular test', function (assert) {
     var test = tape.createHarness({ exit : false });
-    var tc = tap.createConsumer();
+    assert.plan(1);
 
-    var rows = [];
-    tc.on('data', function (r) { rows.push(r) });
-    tc.on('end', function () {
-        // console.log("rs", rows)
-
-        // console.log("deepEqual?")
-
-        assert.same(rows, [
-            "TAP version 13"
-            , "circular"
-            , { id: 1
-                , ok: false
-                , name: " should be equal"
-                , operator: "equal"
-                , expected: "{}"
-                , actual: '{ circular: [Circular] }'
-            }
-            , "tests 1"
-            , "pass  0"
-            , "fail  1"
-        ])
-        assert.end()
-    })
-
-    // tt.equal(10, 10)
-    // tt.end()
-
-    test.createStream().pipe(tc);
+    test.createStream().pipe(concat(function (body) {
+        assert.equal(
+            body.toString('utf8'),
+            'TAP version 13\n'
+            + '# circular\n'
+            + 'not ok 1 should be equal\n'
+            + '  ---\n'
+            + '    operator: equal\n'
+            + '    expected: |-\n'
+            + '      {}\n'
+            + '    actual: |-\n'
+            + '      { circular: [Circular] }\n'
+            + '  ...\n'
+            + '\n'
+            + '1..1\n'
+            + '# tests 1\n'
+            + '# pass  0\n'
+            + '# fail  1\n'
+        );
+    }));
 
     test("circular", function (t) {
-        t.plan(1)
-        var circular = {}
-        circular.circular = circular
-        t.equal(circular, {})
+        t.plan(1);
+        var circular = {};
+        circular.circular = circular;
+        t.equal(circular, {});
     })
 })
