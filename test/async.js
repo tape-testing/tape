c8644346574472e1f3d2eb3bc734d0a4273f0de8
Kory Nunn <knunn187@gmail.com>
2016-01-11 10:31:50 +1000

    [Fix] Resolves issue where tests would not attempt to complete

---
+++ test/async.js
@@ -0,0 +1,37 @@
+var tape = require('../');
+var tap = require('tap');
+
+tap.test('async test calls', function (tt) {
+    tt.plan(1);
+
+    var test = tape.createHarness();
+    var tc = tap.createConsumer();
+
+    test.createStream().pipe(tc);
+
+    function run1(callback){
+        test('first', function (t) {
+            t.plan(1);
+
+            t.pass();
+
+            setTimeout(callback, 10);
+        });
+    }
+
+    function run2(callback){
+        test('second', function (t) {
+            t.plan(1);
+
+            t.pass();
+
+            setTimeout(callback, 10);
+        });
+    }
+
+    run1(function(){
+        run2(function(){
+            tt.pass();
+        });
+    });
+});
