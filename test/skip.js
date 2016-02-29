1414948acaeb0a3b39e77b78a0ea94725756142c
Jordan Harband <ljharb@gmail.com>
2016-02-28 23:49:32 -0800

    [New] Skipped test blocks should output a “SKIP” message.

--- test/skip.js
+++ test/skip.js
@@ -1,6 +1,34 @@
 var test = require('../');
 var ran = 0;
 
+var concat = require('concat-stream');
+var tap = require('tap');
+
+tap.test('test SKIP comment', function (assert) {
+    assert.plan(1);
+
+    var verify = function (output) {
+        assert.equal(output.toString('utf8'), [
+            'TAP version 13',
+            '# SKIP skipped',
+            '',
+            '1..0',
+            '# tests 0',
+            '# pass  0',
+            '',
+            '# ok',
+            ''
+        ].join('\n'));
+    };
+
+    var tapeTest = test.createHarness();
+    tapeTest.createStream().pipe(concat(verify));
+    tapeTest('skipped', { skip: true }, function (t) {
+        t.end();
+    });
+});
+
+
 test('do not skip this', { skip: false }, function(t) {
     t.pass('this should run');
     ran ++;
