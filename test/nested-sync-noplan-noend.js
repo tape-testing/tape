370b6fb73315b657ee55b2107a489866969c8f6b
nelsonic <contact.nelsonic@gmail.com>
2016-08-28 07:26:43 +0100

    update test/nested-sync-noplan-noend.js to use concat-stream instead of tap.createConsumer (no longer available) #312

--- test/nested-sync-noplan-noend.js
+++ test/nested-sync-noplan-noend.js
@@ -1,37 +1,33 @@
 var tape = require('../');
 var tap = require('tap');
-var trim = require('string.prototype.trim');
+var concat = require('concat-stream');
 
 tap.test('nested sync test without plan or end', function (tt) {
     tt.plan(1);
 
     var test = tape.createHarness();
-    var tc = tap.createConsumer();
+    var tc = function (rows) {
 
-    var rows = [];
-    tc.on('data', function (r) { rows.push(r) });
-    tc.on('end', function () {
-        var rs = rows.map(function (r) {
-            if (r && typeof r === 'object') {
-                return { id : r.id, ok : r.ok, name : trim(r.name) };
-            }
-            else return r;
-        });
+        var rs = rows.toString('utf8').split('\n');
         var expected = [
             'TAP version 13',
-            'nested without plan or end',
-            'first',
-            { id: 1, ok: true, name: 'should be truthy' },
-            'second',
-            { id: 2, ok: true, name: 'should be truthy' },
-            'tests 2',
-            'pass  2',
-            'ok'
+            '# nested without plan or end',
+            '# first',
+            'ok 1 should be truthy',
+            '# second',
+            'ok 2 should be truthy',
+            '',
+            '1..2',
+            '# tests 2',
+            '# pass  2',
+            '',
+            '# ok',
+            ''
         ]
         tt.same(rs, expected);
-    });
+    };
 
-    test.createStream().pipe(tc);
+    test.createStream().pipe(concat(tc));
 
     test('nested without plan or end', function(t) {
         t.test('first', function(q) {
