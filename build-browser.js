a3e046673e5a9ffec988fdd7da45f899d36d0312
Arindam Bose <arindam.bose@mapbox.com>
2019-07-25 13:48:45 -0700

    Add a browser field and bundle script

---
+++ build-browser.js
@@ -0,0 +1,35 @@
+var browserify = require('browserify');
+var fs = require('fs');
+
+var BUNDLE_INPUT = 'index.js';
+var BUNDLE_OUTPUT = './dist/bundle.js';
+
+//Helper method that modifies package.json on disk.
+function modifyPackageJson(cb) {
+    var packageStr = fs.readFileSync('package.json', { encoding: 'utf8' });
+    var packageJson = JSON.parse(packageStr);
+    var modifiedPackageJson  = cb(packageJson);
+    packageStr = JSON.stringify(modifiedPackageJson, null, 4);
+    fs.writeFileSync('package.json', packageStr, { encoding: 'utf8' });
+}
+
+// Having the `browser` field in package.json breaks browserify,
+// Temporarily remove it so we can run the bundle
+modifyPackageJson(function(packageJson) {
+    var clone = JSON.parse(JSON.stringify(packageJson));
+    delete clone.browser;
+    return clone;
+});
+
+var b = browserify()
+    .add(BUNDLE_INPUT)
+    .bundle((err, buff) => {
+        if(err) { throw err; }
+
+        fs.writeFileSync(BUNDLE_OUTPUT, buff, { encoding: 'utf8' });
+        modifyPackageJson(function(packageJson) {
+            var clone = JSON.parse(JSON.stringify(packageJson));
+            clone["browser"] = BUNDLE_OUTPUT;
+            return clone;
+        });
+    })
