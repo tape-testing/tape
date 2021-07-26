034ce6ae27660bc9d0cffce649b6a36bf36cd269
Jordan Harband <ljharb@gmail.com>
2021-07-26 14:52:58 -0700

    [Tests] add failing test for #425

--- test/onFinish.js
+++ test/onFinish.js
@@ -3,12 +3,26 @@
 var tap = require('tap');
 var tape = require('../');
 
-tap.test('on finish', { timeout: 1000 }, function (tt) {
+tap.test('on finish', { timeout: 1e3 }, function (tt) {
 	tt.plan(1);
 	tape.onFinish(function () {
 		tt.pass('tape ended');
 	});
 	tape('dummy test', function (t) {
+		t.comment('default harness comment');
+		t.end();
+	});
+});
+
+var tapeHarness = tape.createHarness();
+
+tap.test('on finish (createHarness)', { timeout: 1e3 }, function (tt) {
+	tt.plan(1);
+	tapeHarness.onFinish(function () {
+		tt.pass('tape ended');
+	});
+	tapeHarness('dummy test', function (t) {
+		t.comment('createHarness comment');
 		t.end();
 	});
 });
